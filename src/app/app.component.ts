4d61d1aa7a60eab7de119d266b82c5753960e979
Alain <alainstoicanu@gmail.com>
2018-10-04 00:21:23 +0200

    integrate firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Test';
+  items: Observable<{}>;
+  collection;
+
+  constructor(db: AngularFirestore) {
+    this.collection = db.collection('/test');
+    this.items = db.collection('/test').valueChanges();
+  }
+
+  addItem(item) {
+    this.collection.add(item);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
-
+import { AngularFirestore } from '@angular/fire/firestore';
+const config = {
+  apiKey: 'AIzaSyDhz6rXJv4QThfryYCD2htDMoc7TNv1S7k',
+  authDomain: 'test-96e8b.firebaseapp.com',
+  databaseURL: 'https://test-96e8b.firebaseio.com',
+  projectId: 'test-96e8b',
+  storageBucket: 'test-96e8b.appspot.com',
+  messagingSenderId: '257591783536'
+};
 @NgModule({
   declarations: [
     AppComponent,
     HelloWorldComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule
   ],
-  providers: [],
+  providers: [AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
